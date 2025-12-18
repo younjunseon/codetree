0b70e9cad8e15818e6ea1f8c41451e2e269f0e94
younjunseon <yjs021027@naver.com>
2025-12-19 02:05:35 +0900

    Title: 사칙연산 연습, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251219/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 30;
+    int c;
+    c= a+b;
+    int m;
+    int n;
+
+    m=c/8;
+    n=c%8;
+    cout<<m;
+    cout<<"\n";
+    cout<<n;
+    return 0;
+}
