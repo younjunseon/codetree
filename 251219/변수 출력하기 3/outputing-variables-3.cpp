9461a5bedd556202434f5d89b4be88a1f548c3e0
younjunseon <yjs021027@naver.com>
2025-12-19 02:07:48 +0900

    Title: 변수 출력하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251219/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 1;
+    int b = 2;
+    char c = 'C';
+
+    cout<<a;
+    cout<<"->";
+    cout<<b;
+    cout<<"->";
+    cout<<c;
+    return 0;
+}
