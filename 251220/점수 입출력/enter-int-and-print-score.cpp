a66e9e7674e59e63c9e01ca7811246f4864a5a8c
younjunseon <yjs021027@naver.com>
2025-12-20 21:23:58 +0900

    Title: 점수 입출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251220/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+
+    cout<<"Your score is "<<a<<" point.";
+    return 0;
+}
