3d6e8682d26a5c8c07b22c831c3576d91b790f40
younjunseon <yjs021027@naver.com>
2025-12-21 20:23:26 +0900

    Title: 정수의 조건 여부, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251221/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    if(a>=113){
+        cout<<"1";
+    }
+    else{
+        cout<<"0";
+    }
+    return 0;
+}
