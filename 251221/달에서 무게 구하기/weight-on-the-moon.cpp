d572e67deae810ce2647f019126554083309c953
younjunseon <yjs021027@naver.com>
2025-12-21 20:28:35 +0900

    Title: 달에서 무게 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251221/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    double b;
+    a= 13;
+    
+    b= 0.165000;
+    cout<<fixed;
+    cout.precision(6);
+    cout<<a<<" * "<<b<<" = "<<a*b;
+    return 0;
+}
