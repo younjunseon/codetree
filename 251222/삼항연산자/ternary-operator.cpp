466e8b3f0188a381a58fb11cde92b65a6c8161d2
younjunseon <yjs021027@naver.com>
2025-12-22 09:42:47 +0900

    Title: 삼항연산자, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251222/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    return 0;
+    int a;
+    cin>>a;
+
+    if(a=100){
+        cout<<"pass";
+    }
+    else{
+        cout"failure";
+    }
+}
