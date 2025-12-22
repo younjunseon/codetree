38923e620e974473ae3179da97e579e10d0c1730
younjunseon <yjs021027@naver.com>
2025-12-22 09:39:26 +0900

    Title: 2개 중 최대, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251222/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a,b;
+    cin>>a>>b;
+
+    if(a>b){
+        printf(a);
+    }
+    else{
+        printf(b);
+    }
+    return 0;
+}
