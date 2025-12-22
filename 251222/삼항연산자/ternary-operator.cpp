07e52d21b9478f79d28b01d9b59ff8da14120340
younjunseon <yjs021027@naver.com>
2025-12-22 09:44:50 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251222/삼항연산자/ternary-operator.cpp
+++ 251222/삼항연산자/ternary-operator.cpp
@@ -3,14 +3,16 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    return 0;
+    
     int a;
     cin>>a;
 
-    if(a = 100){
+    if(a == 100){
         cout<<"pass";
     }
     else{
         cout<<"failure";
     }
+
+    return 0;
 }
