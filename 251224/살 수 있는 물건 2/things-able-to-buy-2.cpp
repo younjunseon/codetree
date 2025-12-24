1d57e3e12aa906d01d8ab73025e7a748fc4a2459
younjunseon <yjs021027@naver.com>
2025-12-24 12:41:28 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251224/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a
+
+    if(a>=3000){
+        cout<<"book";
+    }
+    else if(a>=1000){
+        cout<<"mask";
+    }
+    else if(a>=500){
+        cout<<"pen";
+    }
+    else{
+        cout<<"no";
+    }
+    return 0;
+}
