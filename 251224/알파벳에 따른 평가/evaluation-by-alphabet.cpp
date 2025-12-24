d88b1f65446e434160c8080e488d1ce3dc6cb0e3
younjunseon <yjs021027@naver.com>
2025-12-24 12:38:01 +0900

    Title: 알파벳에 따른 평가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251224/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a;
+    cin>>a;
+
+    if(a == "S"){
+        cout<<"Superior";
+    }
+    else if(a == "A"){
+        cout<<"Excellent";
+    }
+    else if(a == "B"){
+        cout<<"Good";
+    }
+    else if(a == "C"){
+        cout<<"Usually";
+    }
+    else if(a == "D"){
+        cout<<"Effort";
+    }
+    else{
+        cout<<"Failure";
+    }
+    return 0;
+}
