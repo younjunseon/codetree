1d5481cd6aa4adefe47517d6184de74e41f8559f
younjunseon <yjs021027@naver.com>
2025-12-24 12:29:26 +0900

    Title: 출석 부르기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251224/출석 부르기/calling-attendance.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+
+    if(a==1){
+        cout<<"John";
+    }
+    else if(a==2){
+        cout<<"Tom";
+    }
+    else if(a==3){
+        cout<<"Paul";
+    }
+    else{
+        cout<<"Vacancy";
+    }
+    return 0;
+}
