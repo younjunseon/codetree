c053db1e7fffabf15aa1465ba3a88d253c861230
younjunseon <yjs021027@naver.com>
2025-12-29 22:44:17 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251229/문자열 출력하기/print-string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a;
+    cin>>a;
+    int i;
+    for(i=0;i<a;i++){
+        cout<<"LeebrosCode\n";
+    }
+    return 0;
+}
