9ec721b9f7fe3b109278560988cd63853daec62f
younjunseon <yjs021027@naver.com>
2025-12-29 22:43:01 +0900

    Title: 문자 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251229/문자 출력하기/print-text.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    char a;
+    cin>>a;
+    int i;
+
+    for(i = 0;i<8;i++){
+        cout<<a;
+    }
+    return 0;
+}
