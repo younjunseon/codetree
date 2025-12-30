b2f660a35f3b773b8dc0ec72ceb78d30555d92df
younjunseon <yjs021027@naver.com>
2025-12-30 18:43:22 +0900

    Title: 정수 입력받아 배수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251230/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+
+    for (int i = 1;i<6;i++){
+        cout<<a*i<<" ";
+    }
+    return 0;
+}
