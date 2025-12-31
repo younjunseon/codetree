7b1d5a3af6f24a9ab18edcbcaf2b5142d9af65e9
younjunseon <yjs021027@naver.com>
2025-12-31 18:13:00 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251231/점수 비교/score-comparison.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c,d;
+    cin>>a>>b;
+    cin>>c>>d;
+
+    if(a>c && a>d){
+        cout<<"1";
+    }
+    else{
+        cout<<"0";
+    }
+    return 0;
+}
