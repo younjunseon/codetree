fad7c96fd0ad53c137e5fb4c3fe6a391d6cf641a
younjunseon <yjs021027@naver.com>
2026-01-02 10:45:25 +0900

    Title: 더 긴 문자열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string a;
+    string b;
+
+    cin>>a>>b;
+
+    if(a.length()>b.length()){
+        cout<<a<<" "<<a.length();
+    }
+    else if(a.length()<b.length()){
+        cout<<b<<" "<<b.length();
+    }
+    else{
+        cout<<"same";
+    }
+
+    return 0;
+}
