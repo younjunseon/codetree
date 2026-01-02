8f786ef2939369bddaa3e67f16e265a28a296811
younjunseon <yjs021027@naver.com>
2026-01-02 10:35:19 +0900

    Title: 따옴표 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260102/따옴표 출력/print-quote.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string a = "He says \"It's a really simple sentence\".";
+
+    cout<<a;
+    return 0;
+}
