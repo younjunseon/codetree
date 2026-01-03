733f1503d13e1c43a6ed8908e55c35ced18c169a
younjunseon <yjs021027@naver.com>
2026-01-03 23:28:41 +0900

    Title: 100점 도달하기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260103/100점 도달하기/reach-100-point.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+
+    for(a; a<101;a++){
+        if(a>=90){
+            cout<<"A ";
+        }
+        else if(a>=80){
+            cout<<"B ";
+        }
+        else if(a>=70){
+            cout<<"C ";
+        }
+        else if(a>=60){
+            cout<<"D ";
+        }
+        else{
+            cout<<"F ";
+        }
+    }
+    return 0;
+}
