06e0391bc12cf766ebc126211216420bb69cc231
younjunseon <yjs021027@naver.com>
2026-01-03 23:41:39 +0900

    Title: 1부터의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260103/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    int sum;
+    sum = 0;
+
+    for(int i=0;i<=100;i++){
+        sum += i;
+        if(sum>a){
+            cout<<i;
+            break;
+        }
+    }
+    return 0;
+}
