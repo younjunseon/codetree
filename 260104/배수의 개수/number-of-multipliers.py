a26e89dcf07dffe63fe6f80266898527ecbc0033
younjunseon <yjs021027@naver.com>
2026-01-04 17:35:39 +0900

    Title: 배수의 개수, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260104/배수의 개수/number-of-multipliers.py
+++ 260104/배수의 개수/number-of-multipliers.py
@@ -1,15 +1,15 @@
 a = []
 
 for i in range(10):
-    a[i] =  input().split("\n")
+    a.append(input())
 
 cnt1 = 0
 cnt2 = 0
 
 for i in range(10):
-    if(a[i]%3 == 0):
+    if(int(a[i])%3 == 0):
         cnt1+=1
-    if(a[i]%5 == 0):
+    if(int(a[i])%5 == 0):
         cnt2+=1
 
-print(cnt1+" "+cnt2)
+print(str(cnt1)+" "+str(cnt2))
