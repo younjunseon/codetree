c3f9bfaad23b69dc6c2fb94278c18fade01fb911
younjunseon <yjs021027@naver.com>
2026-01-06 21:09:04 +0900

    Title: 직각 삼각형 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260106/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+
+    for(int i = 1;i<a+1;i++){
+        for(int j=1;j<i*2;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
