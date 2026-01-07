650f803c32eb128d657e366be8fe5b51295a9c0d
younjunseon <yjs021027@naver.com>
2026-01-07 20:55:23 +0900

    Title: 숫자 피라미드, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260107/숫자 피라미드/number-pyramid.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+
+    for(int i = 1;i<a+1;<i++){
+        for(int j = 1;j<i+1;j++){
+            cout<<i<<" ";
+        }
+        cout<<"\n";
+
+    }
+    return 0;
+}
