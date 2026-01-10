a369e07db002d532c25012caf6c302029b29f66a
younjunseon <yjs021027@naver.com>
2026-01-10 21:04:48 +0900

    Title: 상하로 반복하며 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260110/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a;
+    cin>>a;
+    int b = 1
+
+    for(int i = 0; i<a; i++){
+        if(i%2 == 0){
+            int b = 1;
+                for(int j = 0; j<a; j++){
+                a[j][i] = b;
+                b += 1;
+            }
+        }
+        else{
+            b = a;
+               for(int j = 0; j<a; j++){
+                    a[j][i] = b;
+                    b -= 1;
+               }
+        }
+    }
+
+    for(i = 0; i<a; i++){
+        for(j = 0; j<a; j++){
+            cout<<a[j][i];
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
